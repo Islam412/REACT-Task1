86d35d9340bb3105cf0dde08e35f4669303da8ed
Islam412 <islam.124.hamdy@gmail.com>
2024-10-05 02:38:49 +0300

    add header

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <div style={{width: "100%", backgroundColor: "teal", height: "200px", color: "white", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h1>Islam Hamdy</h1>
+        </div>
+    )
+}
+
+export default Header;
