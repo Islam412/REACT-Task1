d89cb36d3786cd2697204ded4b99af37d169ee4a
Islam412 <islam.124.hamdy@gmail.com>
2024-10-05 04:26:29 +0300

    call button at  anther badge(TagButtom)

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -1,10 +1,20 @@
-import "./TagButtomStyle.css"
+import TagButtom from "./TagButtom"
 
 
 export default function(){
     return (
         <div style={{ margin: "25px", border: "solid teal 5px" }}>
-            <button className="tagButton" style={{  }}>Click Me</button>
+            {/* <button className="tagButton">Click Me</button> call anther badge(TagButtom)*/}
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
+            <TagButtom />
         </div>
     )
 }
