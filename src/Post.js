a0317ef878de0ecf89039403df2b608498829e49
Islam412 <islam.124.hamdy@gmail.com>
2024-10-05 03:18:22 +0300

    add post conntect

---
+++ src/Post.js
@@ -0,0 +1,9 @@
+export default function Post() {
+    return (
+        <div>
+            <h2>This is the post title</h2>
+            <hr />
+            <p>This is the post body</p>
+        </div>
+    );
+}
