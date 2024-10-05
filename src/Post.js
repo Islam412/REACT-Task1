a8ab879e519329149cb0a73485c739d4ab0ad3e0
Islam412 <islam.124.hamdy@gmail.com>
2024-10-05 03:28:04 +0300

    add styel off post conntect

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 export default function Post() {
     return (
-        <div>
+        <div style={{
+            padding: "10px",
+            border: "solid teal 5px",
+            width: "50%",
+            margin: "25px",
+            }}>
             <h2>This is the post title</h2>
             <hr />
             <p>This is the post body</p>
